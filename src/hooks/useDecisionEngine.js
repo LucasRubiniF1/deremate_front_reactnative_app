5645bf52ecffc8e1509c2ed0290d07d91e1ed775
matiasgluck <matiasugluck@gmail.com>
2025-05-06 19:56:21 -0300

    feat: add zustand

--- src/hooks/useDecisionEngine.js
+++ src/hooks/useDecisionEngine.js
@@ -1,3 +1,10 @@
+import useAuthStore from "../store/useAuthStore";
+import {useRootNavigationState} from "expo-router";
+
 export const useDecisionEngine = () => {
+  const { isAuthenticated } = useAuthStore()
+
+  const rootNavigationState = useRootNavigationState()
+
   return {}
 }
