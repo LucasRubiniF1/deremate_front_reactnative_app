0f4cfe54bee1df564f9d642850b5a2b3a1d716ea
Tomas Alvarez <tomasignacioalv@hotmail.com>
2025-05-15 21:49:53 -0300

    feat: add verify and resendCode functions to auth service

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -11,3 +11,19 @@ export const signup = async (data) => {
   const response = await UnauthorizedService.post(`${URL}/signup`, data)
   return response.data
 }
+
+export const verify = async ({ token, email }) => {
+  const response = await UnauthorizedService.post(
+    `${URL}/verify?token=${token}&email=${encodeURIComponent(email)}`
+  );
+  return response.data;
+};
+
+export const resendCode = async ({ email }) => {
+  const response = await UnauthorizedService.post(
+    `${URL}/resend-verification?email=${encodeURIComponent(email)}`
+  );
+  console.log(response, "eee");
+  
+  return response.data;
+};
